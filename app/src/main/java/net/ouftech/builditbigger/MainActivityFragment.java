43f07b7c95a332dfb1f1667e2b025168982aff00
Antoine Purnelle <antoine.purnelle@ouftech.net>
2018-06-07 18:44:17 +0200

    [Part 3] Load jokes from GCE

--- app/src/main/java/net/ouftech/builditbigger/MainActivityFragment.java
+++ app/src/main/java/net/ouftech/builditbigger/MainActivityFragment.java
@@ -1,26 +1,50 @@
+/*
+ * Copyright 2018 Antoine PURNELLE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.ouftech.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
+import net.ouftech.builditbigger.backend.myApi.MyApi;
 import net.ouftech.jokedisplayer.JokeDisplayerActivity;
-import net.ouftech.jokesprovider.JokesProvider;
 import net.ouftech.ouftechcommons.BaseFragment;
+import net.ouftech.ouftechcommons.Logger;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
-import icepick.State;
 
 
 /**
@@ -35,9 +59,8 @@ public class MainActivityFragment extends BaseFragment {
     @BindView(R.id.tell_joke_button)
     Button tellJokeButton;
     Unbinder unbinder;
-    JokesProvider jokesProvider;
-    @State
-    int jokeNumber = 0;
+    @BindView(R.id.pb_loading_indicator)
+    ProgressBar pbLoadingIndicator;
 
     public MainActivityFragment() {
     }
@@ -80,22 +103,66 @@ public class MainActivityFragment extends BaseFragment {
         if (!isRunning())
             return;
 
-        String joke = getJoke();
-
-        Intent intent = new Intent(getActivity(), JokeDisplayerActivity.class);
-        intent.putExtra(JokeDisplayerActivity.JOKE_EXTRA, joke);
-        getActivity().startActivity(intent);
+        new EndpointsAsyncTask().execute(getContext());
     }
 
-    private String getJoke() {
-        if (jokesProvider == null)
-            jokesProvider = new JokesProvider();
+    class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+        private MyApi myApiService = null;
+        private Context context;
 
-        if (jokeNumber == 9)
-            jokeNumber = 0;
-        else
-            jokeNumber++;
+        @NonNull
+        protected String getLogTag() {
+            return "EndpointsAsyncTask";
+        }
 
-        return jokesProvider.getJoke(jokeNumber);
+        @Override
+        protected String doInBackground(Context... params) {
+            setLoading(true);
+
+            if (myApiService == null) {  // Only do this once
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                        new AndroidJsonFactory(), null)
+                        // options for running against local devappserver
+                        // - 10.0.2.2 is localhost's IP address in Android emulator
+                        // - turn off compression when running against local devappserver
+                        .setRootUrl("http://udacity-25d48.appspot.com/_ah/api/")
+                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                            @Override
+                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                                abstractGoogleClientRequest.setDisableGZipContent(true);
+                            }
+                        });
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            context = params[0];
+
+            try {
+                return myApiService.getJoke().execute().getData();
+            } catch (IOException e) {
+                Logger.e(getLogTag(), e);
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (getActivity() == null)
+                return;
+
+            setLoading(false);
+            Intent intent = new Intent(getActivity(), JokeDisplayerActivity.class);
+            intent.putExtra(JokeDisplayerActivity.JOKE_EXTRA, result);
+            getActivity().startActivity(intent);
+        }
+    }
+
+    public void setLoading(boolean loading) {
+        getActivity().runOnUiThread(() -> {
+            pbLoadingIndicator.setVisibility(loading ? View.VISIBLE : View.GONE);
+            tellJokeButton.setEnabled(!loading);
+        });
     }
 }
---
+++ backend/src/main/java/net/ouftech/builditbigger/backend/MyEndpoint.java
@@ -0,0 +1,55 @@
+package net.ouftech.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+
+import net.ouftech.jokesprovider.JokesProvider;
+
+import java.util.Random;
+
+import javax.inject.Named;
+
+/** An endpoint class we are exposing */
+@Api(
+        name = "myApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.ouftech.net",
+                ownerName = "backend.builditbigger.ouftech.net",
+                packagePath = ""
+        )
+)
+public class MyEndpoint {
+
+    JokesProvider jokesProvider;
+
+
+    @ApiMethod(name = "sayHi")
+    public MyBean sayHi(@Named("name") String name) {
+        MyBean response = new MyBean();
+        response.setData("Hi, " + name);
+
+        return response;
+    }
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        MyBean response = new MyBean();
+
+        jokesProvider = new JokesProvider();
+        String joke = retrieveJoke();
+
+        response.setData(joke);
+
+        return response;
+    }
+
+    private String retrieveJoke() {
+        if (jokesProvider == null)
+            jokesProvider = new JokesProvider();
+
+        Random rand =  new Random();
+        return jokesProvider.getJoke(rand.nextInt(10));
+    }
+
+}
